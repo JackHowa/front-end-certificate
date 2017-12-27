2a4d00dcc4814651b149522046c25d5767d340ee
jackhowa <jackhowaa@gmail.com>
2017-12-26 20:09:18 -0600

    add strategy

--- steam-roller.js
+++ steam-roller.js
@@ -1,6 +1,12 @@
 function steamrollArray(arr) {
   const flatArray = [].concat(...arr);
   return Array.isArray(flatArray);
+  // may want to iterate and check through each element is array
+  // then check each
+  // if one is array
+  // then spread?
+  // possibly recurse
+  // or while loop
 }
 
 steamrollArray([1, [2], [3, [[4]]]]);
