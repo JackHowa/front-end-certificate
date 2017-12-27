7c44356a29306f0135a47fb9966d7c8a9efea082
jackhowa <jackhowaa@gmail.com>
2017-12-26 20:01:34 -0600

    account for finding none with the array function

--- drop-it.js
+++ drop-it.js
@@ -1,12 +1,17 @@
 function dropElements(arr, func) {
   const beginCutIndex = arr.findIndex(func);
-  arr.splice(beg);
+  if (beginCutIndex === -1) {
+    return [];
+  }
+  arr.slice(beginCutIndex);
 }
 
 const exampleTest = dropElements([1, 2, 3], function(n) {
-  return n < 2;
+  return n > 5;
 });
 
 console.log(exampleTest);
 
 // https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Array/findIndex
+
+// dropElements([1, 2, 3, 4], function(n) {return n > 5;}) should return [].
