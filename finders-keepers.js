a26487e91eb85a7464afe5f7f481113f7e12f5d3
jackhowa <jackhowaa@gmail.com>
2017-12-26 19:20:54 -0600

    add filter method with tests pass

--- finders-keepers.js
+++ finders-keepers.js
@@ -1,8 +1,19 @@
 function findElement(arr, func) {
-    var num = 0;
-    return num;
+    return arr.filter(func)[0];
 }
 
 findElement([1, 2, 3, 4], function (num) {
     return num % 2 === 0;
 });
+
+const firstTest = findElement([1, 3, 5, 8, 9, 10], function (num) {
+    return num % 2 === 0;
+});
+
+console.assert(firstTest === 8, "First test fails not equal to 8");
+
+const secondTest = findElement([1, 3, 5, 9], function (num) {
+    return num % 2 === 0;
+});
+
+console.assert(secondTest == undefined, "Second test doesn't equal undefined");
