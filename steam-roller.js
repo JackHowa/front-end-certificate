e7c29de6df1123fb030eb229d980e28f30e63fa2
jackhowa <jackhowaa@gmail.com>
2017-12-30 13:36:41 -0600

    Tried to make flat array via push

--- steam-roller.js
+++ steam-roller.js
@@ -1,6 +1,16 @@
 function steamrollArray(arr) {
-  const flatArray = [].concat(...arr);
-  return Array.isArray(flatArray);
+  let flatArray = [];
+
+  for (let index = 0; index < arr.length; index++) {
+    if (arr[index].isArray) {
+      flatArray.push(arr[index][0]);
+    }
+  }
+
+  return flatArray;
+
+
+
   // may want to iterate and check through each element is array
   // then check each
   // if one is array
@@ -9,4 +19,10 @@ function steamrollArray(arr) {
   // or while loop
 }
 
-steamrollArray([1, [2], [3, [[4]]]]);
+const firstTest = steamrollArray([1, [2],
+  [3, [
+    [4]
+  ]]
+]);
+
+console.log(firstTest);
