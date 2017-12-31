036c65aab3e00fbe244a1c4a00123905ddbbcd43
jackhowa <jackhowaa@gmail.com>
2017-12-31 11:43:41 -0600

    Use separation of concerns and output array of letters

--- binary-agents.js
+++ binary-agents.js
@@ -1,13 +1,23 @@
 function binaryAgent(str) {
-  return parseInt(str, 2);
+  const binaryArray = str.split(" ");
+  return binaryArray.map(e => binaryToLetter(e));
 }
 
-const firstLetterTest = binaryAgent("01000001");
+function binaryToLetter(binaryElement) {
+  let decimalNum = parseInt(binaryElement, 2);
+  return String.fromCharCode(decimalNum);
+}
 
-console.log(firstLetterTest); // 䉁 huh? han char
+const firstLetterTest = binaryToLetter("01000001");
 
-binaryAgent(
+console.log(firstLetterTest); // A
+
+const manyLettersTest = binaryAgent(
   "01000001 01110010 01100101 01101110 00100111 01110100 00100000 01100010 01101111 01101110 01100110 01101001 01110010 01100101 01110011 00100000 01100110 01110101 01101110 00100001 00111111"
 );
 
-// The static String.fromCharCode() method returns a string created from the specified sequence of UTF-16 code units.
+console.log(manyLettersTest);
+
+// parseInt(string, radix);
+// radix
+// An integer between 2 and 36 that represents the radix (the base in mathematical numeral systems) of the above mentioned string. Specify 10 for the decimal numeral system commonly used by humans. Always specify this parameter to eliminate reader confusion and to guarantee predictable behavior. Different implementations produce different results when a radix is not specified, usually defaulting the value to 10.
