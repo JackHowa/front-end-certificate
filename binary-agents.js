1468935efacd3fe5aab2b9288037050c120e9cda
jackhowa <jackhowaa@gmail.com>
2017-12-31 11:18:37 -0600

    Returns han chinese char using from char code at huh

--- binary-agents.js
+++ binary-agents.js
@@ -1,7 +1,13 @@
 function binaryAgent(str) {
-  return str;
+  return String.fromCharCode(str);
 }
+
+const firstLetterTest = binaryAgent("01000001");
+
+console.log(firstLetterTest); // 䉁 huh? han char
 
 binaryAgent(
   "01000001 01110010 01100101 01101110 00100111 01110100 00100000 01100010 01101111 01101110 01100110 01101001 01110010 01100101 01110011 00100000 01100110 01110101 01101110 00100001 00111111"
 );
+
+// The static String.fromCharCode() method returns a string created from the specified sequence of UTF-16 code units.
