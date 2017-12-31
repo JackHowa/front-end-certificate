c0b89b377c084f290a147d0338024fa8f27476b1
jackhowa <jackhowaa@gmail.com>
2017-12-31 12:13:41 -0600

    initial commit plus loop through element

---
+++ everything-true.js
@@ -0,0 +1,15 @@
+function truthCheck(collection, pre) {
+  for (let userElement of collection) {
+    console.log(userElement);
+  }
+}
+
+truthCheck(
+  [
+    { user: "Tinky-Winky", sex: "male" },
+    { user: "Dipsy", sex: "male" },
+    { user: "Laa-Laa", sex: "female" },
+    { user: "Po", sex: "female" }
+  ],
+  "sex"
+);
