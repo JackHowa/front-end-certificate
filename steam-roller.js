151b3a4952ab89c6465331a8d98dd4d813c9c0db
jackhowa <jackhowaa@gmail.com>
2017-12-31 11:06:51 -0600

    use recusion and ternary to flatten array

--- steam-roller.js
+++ steam-roller.js
@@ -1,21 +1,22 @@
-function steamrollArray(arr) {
-  let flatArray = [];
-
-  for (let index = 0; index < arr.length; index++) {
-    if (Array.isArray(arr[index])) {
-      if (Array.isArray(arr[index][0])) {
-        console.log("array of array");
-        flatArray.push(arr[index][0][0]);
-      } else {
-        flatArray.push(arr[index][0]);
-      }
-    } else {
-      flatArray.push(arr[index]);
-    }
+function steamrollArray(arr, current = []) {
+  for (let element of arr) {
+    Array.isArray(element) ? steamrollArray(element, current) : current.push(element);
   }
-
-  return flatArray;
+  return current;
 }
+
+
+// for of 
+// https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Statements/for...of
+// for (variable of iterable) {
+//  statement
+// }
+
+
+// ternary https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Operators/Conditional_Operator
+// condition ? expr1 : expr2 
+// 'The fee is ' + (isMember ? '$2.00' : '$10.00');
+
 
 const firstTest = steamrollArray([1, [2],
   [3, [
