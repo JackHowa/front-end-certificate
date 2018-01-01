19cb85dcea768c5ecb51e270902f3dd82f298d9d
jackhowa <jackhowaa@gmail.com>
2018-01-01 11:04:16 -0600

    Make test case as arguments increase and testing the first one as int

--- args-optional.js
+++ args-optional.js
@@ -1,16 +1,24 @@
 function addTogether() {
-  if (!(Number.isInteger(arguments[0]) && Number.isInteger(arguments[1]))) {
-    return undefined;
+  if (arguments.length === 2) {
+    if (!(Number.isInteger(arguments[0]) && Number.isInteger(arguments[1]))) {
+      return undefined;
+    } else {
+      return arguments[0] + arguments[1];
+    }
+  } else if (arguments.length === 1) {
+    if (!(Number.isInteger(arguments[0]))) {
+      // addTogether("http://bit.ly/IqT6zt") should return undefined.
+      return undefined;
+    } else {
+      // ok have to accomodate for clojures 
+      return function (extraArg) {
+        // addTogether(2, "3") should return undefined.
+        if (!(Number.isInteger(arguments[0]))) {
+          return undefined;
+        }
+      };
+    }
   }
-  let addMoreFunction = function addMoreTogether(secondArgument) {
-    return secondArgument;
-  };
-
-  return arguments[0] + addMoreFunction;
-}
-
-function checkInteger(potentialInteger) {
-  return Number.isInteger(potentialInteger);
 }
 
 // It has to add two numbers passed as parameters and return the sum.
@@ -24,14 +32,9 @@ const oneInvalidArg = addTogether(2, "3");
 
 console.log("should be undefined:", oneInvalidArg);
 
-// const testOneValid = addTogether(2)(3);
+const oneExtraArg = addTogether(2)(3);
 
-// console.log(testTwoValidArgs);
+console.log("one extra arg should return five:", oneExtraArg);
 
 // It has to check if it has one or two arguments passed. More are ignored.
 // If it has only one argument then it has to return a function that uses that number and expects another one, to then add it.
-
-// var launchMissiles = function(value) {
-//   return value;
-// };
-// console.log(launchMissiles(4)); returns 4
