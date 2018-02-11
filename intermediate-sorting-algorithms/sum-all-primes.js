9a8e3548ce034c673cd96dafc62b3ff16e95a9d6
jackhowa <jackhowaa@gmail.com>
2018-02-11 10:17:02 -0600

    Setup prime list maker up to a num

--- intermediate-sorting-algorithms/sum-all-primes.js
+++ intermediate-sorting-algorithms/sum-all-primes.js
@@ -1,18 +1,29 @@
 function sumPrimes(num) {
 	let primes = [];
-	i = 1;
-	while (i <= num) {
-		// if index is not divisible by any numbers up to it 
-		// then that current index goes in the array
-		
 
+	// exclude 0, 1 from qualifying 
+	for (let potentialPrime = 2; potentialPrime <= num; potentialPrime++) {
+		// hope for the best 
+		// if set false
+		// then don't add num to array
+		let prime = true;
 
+		// exclude 0, 1
+		// stop at itself 
+		for (let potentialMultiple = 2; potentialMultiple < potentialPrime; potentialMultiple++) {
+			if (potentialPrime % potentialMultiple == 0) {
+				prime = false;
+			}
+		}
 
+		if (prime == true) {
+			primes.push(potentialPrime);
+		}
 	}
-  
+	return primes;
 }
 
-sumPrimes(10);
+console.log(sumPrimes(10));
 
 // 2, 3, 5, 7, 11, 13, 17, 19, 23, 29, 31, 37, 41, 43, 47, 53, 59, 61, 67, 71, 73, 79, 83, 89, 97, 101, 103, 107, 109, 113, 127, 131, 137, 139, 149, 151, 157, 163, 167, 173, 179, 181, 191, 193, 197, 199
 
