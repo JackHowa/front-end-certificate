e6e8148ebcf0bddf5d0f8bbd866dec8da5e23c6e
jackhowa <jackhowaa@gmail.com>
2018-03-08 23:54:51 -0600

    Try out named capture groups

--- advanced-sorting-algorithms/validate-phone-nums.js
+++ advanced-sorting-algorithms/validate-phone-nums.js
@@ -1,8 +1,16 @@
 function telephoneCheck(str) {
-  const firstThreeNums = RegExp('/^\d{3}/', 'g'); // first three characters are digits
-  return str.match(firstThreeNums) ? true : false; // use ternary
+
+	let regex = /(?<areaCode>\d{3})/u;
+	let result = str.replace(regex, (...args) => {
+		let {areaCode} = args[args.length - 1];
+		return `{$areaCode}`;
+	});
+
+  // const firstThreeNums = RegExp('/^\d{3}/', 'g'); // first three characters are digits
+  // return str.match(firstThreeNums) ? true : false; // use ternary
    
   
 }
 
-telephoneCheck("555-555-5555");
+console.log(telephoneCheck("555-555-5555"));
+
