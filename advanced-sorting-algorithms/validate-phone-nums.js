7d962e7fc86e942cafb8ba9ab57733e9842edbf5
jackhowa <jackhowaa@gmail.com>
2018-03-09 22:03:03 -0600

    Everything looked for except conditional regex

--- advanced-sorting-algorithms/validate-phone-nums.js
+++ advanced-sorting-algorithms/validate-phone-nums.js
@@ -1,15 +1,32 @@
 function telephoneCheck(str) {
 	let threeDigits = "\\d{3}";
 	let fourDigits = "\\d{4}";
-	let optionalCountrySpace = "\(" + "\\d{1} " + "\|\)";
 
-	let regexString = "^" + optionalCountrySpace + threeDigits + "-" + threeDigits + "-" + fourDigits;
+	// covers only us, "1", and "1 "
+	let optionalCountrySpace = "\(" + "\[1\] " + "\|" + "\[1\]" + "\|\)";
+
+	let openParenthesis = "\(" + "\[\(\]" + "\|" + "\)";
+
+	let enclosingParenthesis = "\(" + "\[\)\] " + "\|" + "\[\)\]" + "\|-" + "\|" + " \|" + "\)";
+
+	let optionalDash = "\(" + "-" + "\|" + " \|" + "\)";
+
+	let regexString = "^" + optionalCountrySpace + openParenthesis + threeDigits + enclosingParenthesis + threeDigits + optionalDash + fourDigits + "$";
 
 	var re = new RegExp(regexString, 'g');
-	
+
 	return str.match(re) ? true : false;
 }
 
 console.log(telephoneCheck("555-555-5555"));
 console.log(telephoneCheck("1 555-555-5555")); 
-console.log(telephoneCheck("1 (555) 555-5555"));
+console.log(telephoneCheck("1555-555-5555"));
+console.log(telephoneCheck("1 (555) 555-5555")); 
+console.log(telephoneCheck("5555555555"));
+console.log(telephoneCheck("(555)555-5555"));
+
+// may have to partial conditional logic if parens then enclosing parens 
+console.log(telephoneCheck("1 555)555-5555")); // should return false
+
+
+console.log(telephoneCheck("1555 555 5555"));
