0eb220f3391ebea110df3a53c86af178663da58e
jackhowa <jackhowaa@gmail.com>
2018-03-10 18:51:02 -0600

    Completed with odd checks perhaps the result of reversing

--- advanced-sorting-algorithms/validate-phone-nums.js
+++ advanced-sorting-algorithms/validate-phone-nums.js
@@ -15,17 +15,33 @@ function telephoneCheck(str) {
 
 	let re = new RegExp(regexString, 'g');
 
-
-	// could make sure that this is and 
-	// return (str.match(re) ? true : false) && checkParenPairs(str);
-	return (str.match(re) ? true : false)
+	// if there's parenthesis
+	if (typeof checkParenPairs(str) === "boolean") {
+			// could make sure that this is and 
+			// return (str.match(re) ? true : false) && checkParenPairs(str);
+			return (str.match(re) ? true : false) && checkParenPairs(str);
+	} else {
+		return str.match(re) ? true : false
+	}
 }
 
-// (a)?b(?(1)c|d)
 function checkParenPairs(str) {
-	let string = "\(" + 
-	let re = new RegExp("\(?\((\))[)]", 'g');
-	return str.match(re) ? true : false;
+	let openingParenthesis = "\[\(\]";
+	let enclosingParenthesis = "\[\)\]";
+
+	if (str.match(openingParenthesis)) {
+		let checkMatchingParenthesises = "\\((?=\\d{3}\\)\)";
+		console.log(str.match(checkMatchingParenthesises));
+		return str.match(checkMatchingParenthesises) ? true : false;
+	} else if (str.match(enclosingParenthesis)) {
+		// reversing it won't reverse the direction of the parenthesis :( 
+		let reversedStr = str.split("").reverse().join("");
+		let checkMatchingParenthesises = "\\((?=\\d{3}\)";
+		return reversedStr.match(checkMatchingParenthesises) ? true : false;
+	} else {
+		// no parens found
+		return;
+	}
 }
 
 // may just need to hardcode 
@@ -33,17 +49,19 @@ function checkParenPairs(str) {
 // have to ensure that this remains false though 
 // telephoneCheck("(555)5(55?)-5555")
 
+console.log(telephoneCheck("(555-555-5555"));
 
 
-console.log(telephoneCheck("555-555-5555"));
-console.log(telephoneCheck("1 555-555-5555")); 
-console.log(telephoneCheck("1555-555-5555"));
-console.log(telephoneCheck("1 (555) 555-5555")); 
-console.log(telephoneCheck("5555555555"));
-console.log(telephoneCheck("(555)555-5555"));
+
+// console.log(telephoneCheck("555-555-5555"));
+// console.log(telephoneCheck("1 555-555-5555")); 
+// console.log(telephoneCheck("1555-555-5555"));
+// console.log(telephoneCheck("1 (555) 555-5555")); 
+// console.log(telephoneCheck("5555555555"));
+// console.log(telephoneCheck("(555)555-5555"));
 
 // may have to partial conditional logic if parens then enclosing parens 
 console.log(telephoneCheck("1 555)555-5555")); // should return false
 
 
-console.log(telephoneCheck("1555 555 5555"));
+// console.log(telephoneCheck("1555 555 5555"));
