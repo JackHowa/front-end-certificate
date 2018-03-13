78f4ebabad56384e29a17fcf5a290f09db4042c9
jackhoward <jhoward@realnets.com>
2018-03-13 21:41:52 +0000

    Return counts of the matched amounts

--- advanced-sorting-algorithms/symmetric-pairs.js
+++ advanced-sorting-algorithms/symmetric-pairs.js
@@ -2,19 +2,37 @@ function sym(args) {
   // flatten arguments 
   // create array of arguments object 
   // use reduce 
+  // make flat array with reduce 
   let flatArray = Array.from(arguments).reduce( (previous, current) => [...previous, ...current]);
-  return flatArray;
+  
+  // count occurences of flat array 
+  let matchesCount = flatArray.reduce( (existingNumbers, currentNumber) => {
+    if (currentNumber in existingNumbers) {
+      // count up the amount of matches 
+      existingNumbers[currentNumber]++;
+    } else {
+      // set the count to 1 
+      existingNumbers[currentNumber] = 1;
+    }
+    return existingNumbers;
+  }, {})
+
+  // then delete any value except for ones that exist only once
+  // return the keys of values as an array who have a 1 count 
+
+  return matchesCount;
 }
+
+// var countedNames = names.reduce(function (allNames, name) { 
+//   if (name in allNames) {
+//     allNames[name]++;
+//   }
+//   else {
+//     allNames[name] = 1;
+//   }
+//   return allNames;
+// }, {});
 
 // isn't a set only allows unique elements 
 // want only one 
 console.log(sym([1, 2, 3], [5, 2, 1, 4])); // should return [3,4,5]
-
-// day 9 or 8? 
-// liked working with dash docs. much clearer knowing the source. great explanaations too
-// didn't like turbulence while coding 
-// feels good to get things done when you wouldn't think it's possible 
-// just gotta push it and document my work. and tweet it
-// also, I encouraged people on the train to work today so that's set
-// probably could have gotten this work done at the airport where I had wifi to push 
-// but this is kind of meditative
