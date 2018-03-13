1d1ff0ad8885099b754a1b760f2bad46cebc1197
jackhoward <jhoward@realnets.com>
2018-03-13 03:28:08 -0500

    Create flat array to then iterate through

--- advanced-sorting-algorithms/symmetric-pairs.js
+++ advanced-sorting-algorithms/symmetric-pairs.js
@@ -9,3 +9,12 @@ function sym(args) {
 // isn't a set only allows unique elements 
 // want only one 
 console.log(sym([1, 2, 3], [5, 2, 1, 4])); // should return [3,4,5]
+
+// day 9 or 8? 
+// liked working with dash docs. much clearer knowing the source. great explanaations too
+// didn't like turbulence while coding 
+// feels good to get things done when you wouldn't think it's possible 
+// just gotta push it and document my work. and tweet it
+// also, I encouraged people on the train to work today so that's set
+// probably could have gotten this work done at the airport where I had wifi to push 
+// but this is kind of meditative
