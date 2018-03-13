2c7ea8e7641c9549bfdc3d1a03a48a1b2ea36bfd
jackhoward <jhoward@realnets.com>
2018-03-12 21:56:23 -0500

    Now just have a flattened array can do more reducing

--- advanced-sorting-algorithms/symmetric-pairs.js
+++ advanced-sorting-algorithms/symmetric-pairs.js
@@ -1,27 +1,9 @@
 function sym(args) {
-  // The arguments object is an Array-like object corresponding to the arguments passed to a function.
-  // arguments object can be converted to an array with es2015 from
-  return Array.from(arguments).reduce( (previous, current) => {
-    // make the previous array to edit it 
-    // let previousArray = Array.from(previous);
-    console.log(previous);
-    // if previous array includes current 
-    // need to find the index of that match
-    let matchIndex = previous.findIndex(current);
-
-    // findIndex returns -1 if no match 
-    if (matchIndex != "-1") {
-      // then delete both the previous array match and current element
-      // oh no I've made a dire mistake, you're not unique, you must go 
-      previousArray.slice(matchIndex, 1);
-      return previousArray;
-    } else {
-      return [...previous, ...current];
-    }  
-  }, 
-  // spread into an empty array initial value
-  []
-  );
+  // flatten arguments 
+  // create array of arguments object 
+  // use reduce 
+  let flatArray = Array.from(arguments).reduce( (previous, current) => [...previous, ...current]);
+  return flatArray;
 }
 
 // isn't a set only allows unique elements 
