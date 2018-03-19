e1dc38847dbb87d3cb39c7ae92a71c137d44d990
jackhowa <jackhowaa@gmail.com>
2018-03-19 06:53:26 +0000

    Use reduce to begin mapping the algorithm for checking currency

--- advanced-sorting-algorithms/exact-change.js
+++ advanced-sorting-algorithms/exact-change.js
@@ -1,7 +1,10 @@
 function checkCashRegister(price, cash, cid) {
-    var change;
-    // Here is your change, ma'am.
-    return change;
+  return cid.reduce( (remainingCash, castPerCurrency) => {
+      let highestCash = cid.pop();
+      remainingCash = highestCash[1] - cash;
+      return remainingCash;
+
+    }, 0)
   }
   
   // Example cash-in-drawer array:
@@ -15,7 +18,7 @@ function checkCashRegister(price, cash, cid) {
   // ["TWENTY", 60.00],
   // ["ONE HUNDRED", 100.00]]
   
-  checkCashRegister(19.50, 20.00, [["PENNY", 1.01], ["NICKEL", 2.05], ["DIME", 3.10], ["QUARTER", 4.25], ["ONE", 90.00], ["FIVE", 55.00], ["TEN", 20.00], ["TWENTY", 60.00], ["ONE HUNDRED", 100.00]]);
+  console.log(checkCashRegister(19.50, 20.00, [["PENNY", 1.01], ["NICKEL", 2.05], ["DIME", 3.10], ["QUARTER", 4.25], ["ONE", 90.00], ["FIVE", 55.00], ["TEN", 20.00], ["TWENTY", 60.00], ["ONE HUNDRED", 100.00]]));
   
 /*
   checkCashRegister(19.50, 20.00, [["PENNY", 1.01], ["NICKEL", 2.05], ["DIME", 3.10], ["QUARTER", 4.25], ["ONE", 90.00], ["FIVE", 55.00], ["TEN", 20.00], ["TWENTY", 60.00], ["ONE HUNDRED", 100.00]]) should return an array.
