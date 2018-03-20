e29d390ddd4fade224ec9b3efa3662a5881b0f3b
jackhowa <jackhowaa@gmail.com>
2018-03-20 22:53:58 +0000

    Add comments for troubleshooting issue with exact bill change

--- advanced-sorting-algorithms/exact-change.js
+++ advanced-sorting-algorithms/exact-change.js
@@ -2,38 +2,41 @@ function checkCashRegister(price, cash, cid) {
   const CONVERSION_SEQUENCE = [100, 20, 10, 5, 1, 0.25, 0.10, 0.05, 0.01];
 
   let conversionIndex = 0;
-  let newCashInDrawer = [];
+  let totalChangePerCategory = [];
 
   let changeToGiveBack = cash - price;
-  for (let i = cid.length; i > 0; i--) {
+  while(changeToGiveBack > 0) {
     // use pop to start at the end of the array of arrays
     // [1,2,4] will start with 4 now
     // [...,["TWENTY", 60.00], ["ONE HUNDRED", 100.00]])]
     let [currency, amountOfCurrency] = cid.pop();
 
-    if (typeof amountOfCurrency === "undefined") {
-      return "Insufficient Funds";
-    }
-
     let howManyCurrency = Math.floor(changeToGiveBack / CONVERSION_SEQUENCE[conversionIndex]); 
 
     let totalAmountPerCurrency = howManyCurrency * CONVERSION_SEQUENCE[conversionIndex];
 
-    if (totalAmountPerCurrency > amountOfCurrency) {
+    console.log(totalAmountPerCurrency);
+
+    // oh, I need to take what's left of the total amount in drawer, not 
+    // all or nothing. just because can't give all 100s for exact
+    // change doesn't mean I need to not give any at all 
+    if (0 < totalAmountPerCurrency && totalAmountPerCurrency < amountOfCurrency) {
       changeToGiveBack -= totalAmountPerCurrency;
-      amountOfCurrency -= totalAmountPerCurrency;
+      
+      // don't really need to update list
+      // amountOfCurrency -= totalAmountPerCurrency;
+      // can use unshift to always add to the front 
+      // this will only go through what we get to
+      // todo: might miss pennies if we get exact change 
+
+      // need to do this ([hey] | [ho, hi]) => [hey, ho, hi] in ruby 
+      totalChangePerCategory.unshift([currency, totalAmountPerCurrency]);
     }
-    
-    // can use unshift to always add to the front 
-    // this will only go through what we get to
-    // todo: might miss pennies if we get exact change 
-
-    // need to do this ([hey] | [ho, hi]) => [hey, ho, hi] in ruby 
-    newCashInDrawer.unshift([currency, amountOfCurrency]);
-
     conversionIndex++;
   }
-  return newCashInDrawer;
+
+  // Otherwise, return change in coin and bills, sorted in highest to lowest order.
+  return totalChangePerCategory;
 }
   
   // Example cash-in-drawer array:
@@ -47,8 +50,9 @@ function checkCashRegister(price, cash, cid) {
   // ["TWENTY", 60.00],
   // ["ONE HUNDRED", 100.00]]
   
-  console.log(checkCashRegister(19.50, 20.00, [["PENNY", 1.01], ["NICKEL", 2.05], ["DIME", 3.10], ["QUARTER", 4.25], ["ONE", 90.00], ["FIVE", 55.00], ["TEN", 20.00], ["TWENTY", 60.00], ["ONE HUNDRED", 100.00]]));
+  // console.log(checkCashRegister(19.50, 20.00, [["PENNY", 1.01], ["NICKEL", 2.05], ["DIME", 3.10], ["QUARTER", 4.25], ["ONE", 90.00], ["FIVE", 55.00], ["TEN", 20.00], ["TWENTY", 60.00], ["ONE HUNDRED", 100.00]]));
   
+  console.log(checkCashRegister(3.26, 100.00, [["PENNY", 1.01], ["NICKEL", 2.05], ["DIME", 3.10], ["QUARTER", 4.25], ["ONE", 90.00], ["FIVE", 55.00], ["TEN", 20.00], ["TWENTY", 60.00], ["ONE HUNDRED", 100.00]]));
 /*
   checkCashRegister(19.50, 20.00, [["PENNY", 1.01], ["NICKEL", 2.05], ["DIME", 3.10], ["QUARTER", 4.25], ["ONE", 90.00], ["FIVE", 55.00], ["TEN", 20.00], ["TWENTY", 60.00], ["ONE HUNDRED", 100.00]]) should return an array.
 checkCashRegister(19.50, 20.00, [["PENNY", 0.01], ["NICKEL", 0], ["DIME", 0], ["QUARTER", 0], ["ONE", 0], ["FIVE", 0], ["TEN", 0], ["TWENTY", 0], ["ONE HUNDRED", 0]]) should return a string.
