0d1b126106e96388838d9465313d17ee4d028146
jackhowa <jackhowaa@gmail.com>
2018-03-28 21:57:05 -0500

    Add error message handling

--- advanced-sorting-algorithms/exact-change.js
+++ advanced-sorting-algorithms/exact-change.js
@@ -1,11 +1,23 @@
 /*jshint esversion: 6 */
 
 // setup an array for cash currency name to value 
-const CASH_VALUE_ASCENDING = [ 0.01, 0.05, 0.1, 0.25, 1, 5, 10, 20, 100 ];
+const CASH_VALUE_ASCENDING = [ 0.01, 0.05, 0.1, 0.25, 1.00, 5.00, 10.00, 20.00, 100.00 ];
 
 function checkCashRegister(price, cash, cid) {
 	// subtract price of item from cash given for item  
 	let cashChange = cash - price;
+
+	let totalCashInDrawer = cid.reduce( (total, currencyNameAndCash) => {
+    return total += currencyNameAndCash[1];
+  }, 0.0);
+
+  totalCashInDrawer = Math.round(totalCashInDrawer * 100) / 100;
+
+  if (totalCashInDrawer < cashChange) {
+    return "Insufficient Funds";
+  } else if (totalCashInDrawer === cashChange) {
+    return "Closed";
+  }
 
 	let registerIndex = 8;
 
@@ -42,12 +54,16 @@ function checkCashRegister(price, cash, cid) {
 		let actualChangeMoney = actualChangeCurrencyQuantity * currencyCashValue;
 
 		// will have to round it too 
+		// enough for basic math
 		actualChangeMoney = Math.round(actualChangeMoney * 100) / 100;
 
 		// check if the change amount is zero 
 		if (actualChangeMoney !== 0) {
 			// put that new change in an array 
-			currencyAndAmount.push([drawerCurrencyName, preciseRound(actualChangeMoney, 2)]);
+			// keep trailing zeroes and round 
+			// need 60.00, even when rounds normally 60 
+			// also need a number 
+			currencyAndAmount.push([drawerCurrencyName, actualChangeMoney]);
 		}
 
 		// running cash change total - new change money amount 
@@ -67,3 +83,5 @@ function checkCashRegister(price, cash, cid) {
 console.log(checkCashRegister(19.50, 20.00, [["PENNY", 1.01], ["NICKEL", 2.05], ["DIME", 3.10], ["QUARTER", 4.25], ["ONE", 90.00], ["FIVE", 55.00], ["TEN", 20.00], ["TWENTY", 60.00], ["ONE HUNDRED", 100.00]]));
 
 console.log(checkCashRegister(3.26, 100.00, [["PENNY", 1.01], ["NICKEL", 2.05], ["DIME", 3.10], ["QUARTER", 4.25], ["ONE", 90.00], ["FIVE", 55.00], ["TEN", 20.00], ["TWENTY", 60.00], ["ONE HUNDRED", 100.00]]));
+
+// float making and round to number via https://stackoverflow.com/a/24889569
