680d08564f054f5cca13986d1076490417093789
jackhowa <jackhowaa@gmail.com>
2018-04-01 12:09:39 -0500

    Add pseudocode and rename inventory variables

--- advanced-sorting-algorithms/inventory-update.js
+++ advanced-sorting-algorithms/inventory-update.js
@@ -1,11 +1,21 @@
-function updateInventory(arr1, arr2) {
-  for (let i = 0; i < arr2.length; i++) {
-    if (!(arr1.includes(arr2[i]))) {
-      arr1.push(arr2[i]);
+function updateInventory(inventoryInitial, inventoryChange) {
+  // loop through all of the inventory change
+  // find the change in inventory's name
+  // compare that first inventory's name to all of the initial inventory
+  // if the inventory change name is the same as that inventory's name
+  // then add that inventory change array's first element amount to the initial change amount
+  // else if the inventory change is not the same as the inventory's name
+  // add that new change to the intial inventory
+  // end loop
+  //
+
+  for (let i = 0; i < inventoryChange.length; i++) {
+    if (!(inventoryInitial.includes(inventoryChange[i]))) {
+      inventoryInitial.push(inventoryChange[i]);
     }
   }
 
-  return arr1;
+  return inventoryInitial;
 }
 
 // Example inventory lists
